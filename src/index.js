dac70883a045300cbf9b021b7192c3a1a8f2459f
Max revolution <maxrevolution8@gmail.com>
2021-07-27 18:00:13 +0200

    My first function :)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Welcome Maxrevolution Hangman Game")
+console.log("Welcome to Maxrevolution Hangman Game");
+
+function suma(a, b){
+    return a + b;
+}
+
+console.log("the sum of 2 + 5 is:", suma(5, 2))
+
+console.log("the sum of 9090 + 45673 is:", suma(9090, 45673))
